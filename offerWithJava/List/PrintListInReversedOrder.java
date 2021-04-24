cdb10c0b01e2f71bc0b7da2bc5793278e8a6e2c4
flyingchase <zhongbingwang@yeah.net>
2021-04-24 16:15:17 +0800

    逆序ListNode--辅助栈+头插法+递归

--- offerWithJava/List/PrintListInReversedOrder.java
+++ offerWithJava/List/PrintListInReversedOrder.java
@@ -14,8 +14,8 @@ import InnerStruct.*;
  * 
  */
 
+
 public class PrintListInReversedOrder {
-    ListNode he = new ListNode(-1);
     
     // 递归 
     public static ArrayList<Integer> print01ReversedOrderListNode(ListNode listNode) {
@@ -60,13 +60,6 @@ public class PrintListInReversedOrder {
     public static void main(String[] args) {
         int[] nums={1,2,3,4,5,6,7,8,9,0};
         Integer[] integers = Arrays.stream(nums).boxed().toArray(Integer[]::new);
-        // ListNode head = new ListNode(integers[0]);
-        // ListNode other =head;
-        // for (int i=1;i<integers.length;i++) {
-        //     ListNode temp = new ListNode(integers[i]);
-        //     other.next=temp;
-        //     other = temp;
-        // }
         ListOperate listOperate = new ListOperate();
 
         ListNode head = ((ListOperate) listOperate).arrayToListNode(nums);
@@ -74,15 +67,7 @@ public class PrintListInReversedOrder {
         ArrayList<Integer> arr = new ArrayList<>();
         arr= print01ReversedOrderListNode(head);
         System.out.println(arr.toString());
-        System.out.println();
-        // arr= print02ReversedOrderListNode(head);
-        // System.out.println(arr.toString());
-        
-        // while (head!=null){
-        //     System.out.print(head.val+" ");
-        //     head=head.next;
-        // }
-        System.out.println();
+       
         arr= print03ReversedOrderListNode(head);
         System.out.println(arr.toString());
     }
