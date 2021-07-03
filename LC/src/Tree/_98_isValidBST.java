4ee8f9beb2d6972549b769dd4a1369fc971c0546
wlzhou <zhongbingwang@yeah.net>
2021-07-03 15:52:06 +0800

    feature(LC-TreeNode):

--- LC/src/Tree/_98_isValidBST.java
+++ LC/src/Tree/_98_isValidBST.java
@@ -1,62 +1,54 @@
 package Tree;
 
 import DataStructure.TreeNode;
+import com.sun.source.tree.Tree;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
+@SuppressWarnings({"all"})
 public class _98_isValidBST {
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(1);
-
-
+        TreeNode root = new TreeNode(4);
         root.left=new TreeNode(2);
-//        root.right=new TreeNode(3);
-        root.left.left=new TreeNode(4);
-//        root.left.right=new TreeNode(5);
-//        root.right.right=new TreeNode(7);
-//        root.right.left=new TreeNode(6);
-//        root.left.left.left=new TreeNode(8);
-//        root.left.left.right=new TreeNode(9);
+        root.right=new TreeNode(6);
+        root.left.left=new TreeNode(1);
+        root.left.right=new TreeNode(3);
+        root.right.right=new TreeNode(7);
+        root.right.left=new TreeNode(5);
 
-        if (isValidBST(root)) {
+        if (isVaildBST(root)) {
             System.out.println("yes");
         }else {
             System.out.println("No");
         }
     }
 
-    public static boolean isValidBST (TreeNode root) {
-return true;
-    }
+    public static boolean isVaildBST (TreeNode root) {
+        boolean flag = true;
+        if (root==null) {
+            return flag;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        Integer preNodeValue = null;
 
-//    public static ArrayList<Integer> inOrderTraversalBST(TreeNode root) {
-//        ArrayList<Integer> res = new ArrayList<>();
-//        if (root==null) {
-//            return res;
-//        }
-//        Stack<TreeNode> stack = new Stack<>();
-//        TreeNode cur=root;
-//        while (cur != null || !stack.isEmpty()) {
-//            while (cur != null) {
-//                stack.push(cur);
-//                cur=cur.left;
-//            }
-//            TreeNode node= stack.pop();
-//            int min = node.val;
-//
-//            res.add(node.val);
-//            cur=node.right;
-//
-//        }
-//
-////        while (root != null || !stack.isEmpty()) {
-////            while (root != null) {
-////                res.add(root.val);
-////                stack.push(root);
-////                root=root.left;
-////            }
-////            TreeNode cur = stack.pop();
-////            root=cur.right;
-////        }
-//    }
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur=cur.left;
+            }
+            TreeNode node = stack.pop();
+            if (preNodeValue!=null&&preNodeValue.intValue()>=node.val) {
+                flag=false;
+                break;
+            } else {
+                preNodeValue=(Integer) node.val;
+            }
+            cur=node.right;
+        }
+        return flag;
+    }
 
 
 }
