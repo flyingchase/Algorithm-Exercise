5d6835a65ed112cafab7598b7d457211b0389626
wlzhou <zhongbingwang@yeah.net>
2021-10-07 15:27:43 +0800

    LC-Go/Sorts/NewMergesort.go

--- LC-Go/Sorts/NewMergesort.go
+++ LC-Go/Sorts/NewMergesort.go
@@ -30,6 +30,42 @@ func mergeNew(nums []int, l int, mid int, r int) {
 	}
 	copy(helper[i:], nums[p1:mid+1])
 	copy(helper[i:], nums[p2:r+1])
-	copy(nums[l : r+1],helper[:])
+	copy(nums[l:r+1], helper[:])
+}
+func quickSortDemo(nums []int) {
+	if len(nums) == 0 {
+		return
+	}
+	quicksortHelper(nums, 0, len(nums)-1)
+}
+func quicksortHelper(nums []int, l, r int) {
+	if l == r {
+		return
+	}
+	p := paration(nums, l, r)
+	quickSort(nums, l, p[0]-1)
+	quickSort(nums, p[1]+1, r)
+}
+
+func paration(nums []int, l, r int) []int {
+	if l == r {
+		return nil
+	}
+	less, more := l-1, r
+	for l < more {
+		if nums[l] > nums[r] {
+			less++
+			nums[less], nums[l] = nums[l], nums[less]
+			l++
+		} else if nums[l] < nums[r] {
+			more--
+			nums[more], nums[l] = nums[l], nums[more]
+		} else {
+			l++
+		}
+	}
+	nums[more], nums[r] = nums[r], nums[more]
+
+	return []int{less + 1, more}
 
 }
