3742c8a117a0089191ebf695a7b3111ce097311d
wlzhou <zhongbingwang@yeah.net>
2021-11-26 23:41:25 +0800

    TopKFromLCTags/Lists/83-removeDuplicatesFromSortedList.go TopKFromLCTags/Lists/lruCacheDemo.go 	TopKFromLCTags/Lists/148-sortList.go 	TopKFromLCTags/Lists/160-getIntersetionNode.go TopKFromLCTags/Lists/19-removenthFromEnd.go 	TopKFromLCTags/Lists/203-removeElements.go TopKFromLCTags/Lists/21-mergeTwoLists.go 	TopKFromLCTags/Lists/23-mergeKLists.go 	TopKFromLCTags/Lists/25-reverseKGroup.go 	TopKFromLCTags/Lists/445-addTwoNumbersII.go 	TopKFromLCTags/Lists/82-deledeDupkicates.go 	TopKFromLCTags/Lists/876-middleNode.go 	TopKFromLCTags/Lists/92-reverseBetween.go

--- TopKFromLCTags/Lists/83-removeDuplicatesFromSortedList.go
+++ TopKFromLCTags/Lists/83-removeDuplicatesFromSortedList.go
@@ -1,16 +1,35 @@
 package topkLists
 
+// iterator
 func deleteDuplicate(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
 	}
 	dummyHead := &ListNode{Val: -1, Next: head}
 	prev, cur := dummyHead, head
-	for cur.Next != nil {
+	prev.Next = cur
+	for cur != nil {
 		next := cur.Next
-		if next.Val == cur.Val {
+		for next != nil && next.Val == cur.Val {
+			next = next.Next
 		}
+		cur.Next = next
+		prev = cur
+		cur = cur.Next
+	}
+	return dummyHead.Next
+}
 
+// recursive
+func deleteDuplicate2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	} else if head.Val == head.Next.Val {
+		head = deleteDuplicate2(head.Next)
+		return head
+	} else {
+		head.Next = deleteDuplicate2(head.Next)
+		return head
 	}
 
 }
