79256c8cbd33c74c30769a2dee410271156c6770
wlzhou <zhongbingwang@yeah.net>
2021-12-11 16:53:54 +0800

    Exercise/8.go Trees/~formatting_0-40_794207_145-postorderTraversal.go.tmp

--- TopKFromLCTags/Exercise/8.go
+++ TopKFromLCTags/Exercise/8.go
@@ -15,10 +15,8 @@ func myAtoi(s string) int {
 	} else if s[i] == '+' {
 		sign = 1
 	}
-
 	i++
 	var res int
-
 	for ; i < len(s); i++ {
 		res = res*10 + int(s[i]-'0')
 		if sign*res < math.MaxInt32 {
@@ -28,7 +26,5 @@ func myAtoi(s string) int {
 			return math.MaxInt32
 		}
 	}
-
 	return res * sign
-
 }
