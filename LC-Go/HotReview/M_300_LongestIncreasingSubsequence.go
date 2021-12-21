d8e28162f8d17aacdd75dad0ab0b1992a06801a0
wlzhou <zhongbingwang@yeah.net>
2021-12-21 11:27:55 +0800

    LC-Go/HotReview/M_300_LongestIncreasingSubsequence.go :   LC-Go/main.go TopKFromLCTags/NotionExerciseRecord/5.go 	interviewAlgRecord/

--- LC-Go/HotReview/M_300_LongestIncreasingSubsequence.go
+++ LC-Go/HotReview/M_300_LongestIncreasingSubsequence.go
@@ -29,22 +29,16 @@ func max300(j int, i int) int {
 	}
 
 }
+
 func lengthOfLIS1(nums []int) int {
-
 	dp := []int{}
-
 	for _, num := range nums {
-
 		i := sort.SearchInts(dp, num)
 		if i == len(dp) {
 			dp = append(dp, num)
 		} else {
-
 			dp[i] = num
-
 		}
-
 	}
 	return len(dp)
-
 }
--- TopKFromLCTags/NotionExerciseRecord/5.go
+++ TopKFromLCTags/NotionExerciseRecord/5.go
@@ -4,7 +4,6 @@ func longestPalindrome(s string) string {
 	if len(s) <= 0 {
 		return s
 	}
-
 	left, right, pl, pr := 0, -1, 0, 0
 	for left < len(s) {
 		for left >= 0 && right+1 < len(s) && s[left] == s[right+1] {
