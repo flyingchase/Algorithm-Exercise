fc38a9c60bbe531d1f70a8e0e52c3c29775b27d4
wlzhou <zhongbingwang@yeah.net>
2021-12-29 22:04:57 +0800

    单调栈总结

--- ImportantAlgAndDataStruct/Alg/monotonousStack.go
+++ ImportantAlgAndDataStruct/Alg/monotonousStack.go
@@ -16,3 +16,20 @@ func nextGreaterElements1(nums []int) []int {
 	}
 	return res
 }
+
+// 单调栈反向模板
+// post process 保存之前 element 的 index
+func nextGreaterElements2(nums []int) []int {
+	n, res, stack := len(nums), make([]int, 0, len(nums)), make([]int, 0)
+	for i, _ := range stack {
+		stack[i] = -1
+	}
+	for i := 0; i < n; i++ {
+		for len(stack) != 0 && nums[stack[len(stack)-1]] < nums[i] {
+			res[stack[len(stack)-1]] = nums[i]
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i)
+	}
+	return res
+}
