d0feb5adb544766f33ce0fa9a7b5e16bafda46a4
wlzhou <zhongbingwang@yeah.net>
2021-12-30 20:09:13 +0800

    Alg/01-binarySearch/33-searchRotate.go 01-binarySearch/81-searchRotateTimes.go 02-doublePoint/

--- ImportantAlgAndDataStruct/Alg/01-binarySearch/33-searchRotate.go
+++ ImportantAlgAndDataStruct/Alg/01-binarySearch/33-searchRotate.go
@@ -1,5 +1,29 @@
 package binarysearch
 
+// 旋转后的数组中找 target
+// 部分有序
 func search(nums []int, target int) int {
-
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if nums[mid] == target {
+			return mid
+		}
+		// 左半部分有序
+		// 注意 left 和 mid 重复情况
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
 }
