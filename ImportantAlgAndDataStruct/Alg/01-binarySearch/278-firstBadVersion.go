87b2c9c015735e253ddebe4d739c12c0b1fd32d9
wlzhou <zhongbingwang@yeah.net>
2022-01-23 21:42:42 +0800

    Alg/01-binarySearch/278-firstBadVersion.go

--- ImportantAlgAndDataStruct/Alg/01-binarySearch/278-firstBadVersion.go
+++ ImportantAlgAndDataStruct/Alg/01-binarySearch/278-firstBadVersion.go
@@ -2,14 +2,5 @@ package binarysearch
 
 // 278 第一个错误的版本
 func firstBadVersion(n int) int {
-	left, right := 1, n
-	for left <= right {
-		// mid:=left+(right-left)>>1
-		// if !isBadVersion(mid) {
-		// 	left=mid+1
-		// }else {
-		// 	right=mid-1
-		// }
-	}
-	return left
+	return 0
 }
