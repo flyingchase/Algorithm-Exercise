e5f18021c6e096398569a5c67f42717a632df322
wlzhou <zhongbingwang@yeah.net>
2022-03-01 09:34:39 +0800

    ../22Intership/lists.go   DataStructure/Array/array.go -> ../Go-DesignPattern/DataStructure/Array/array.go   DataStructure/BinarySearch/template.go -> ../Go-DesignPattern/DataStructure/BinarySearch/template.go   DataStructure/Heap/binaryHeap.go -> ../Go-DesignPattern/DataStructure/Heap/binaryHeap.go   DataStructure/LFU/LFUWithInnerList.go -> ../Go-DesignPattern/DataStructure/LFU/LFUWithInnerList.go   DataStructure/LFU/LFUWithSelf.go -> ../Go-DesignPattern/DataStructure/LFU/LFUWithSelf.go   DataStructure/LRU/LRU.go -> ../Go-DesignPattern/DataStructure/LRU/LRU.go   DataStructure/LinkedList/doublyLinkedlist/doublyLinkedlist.go -> ../Go-DesignPattern/DataStructure/LinkedList/doublyLinkedlist/doublyLinkedlist.go   DataStructure/LinkedList/singlyLinkedlist/singlyLinkedlist.go -> ../Go-DesignPattern/DataStructure/LinkedList/singlyLinkedlist/singlyLinkedlist.go   DataStructure/ListNode.go -> ../Go-DesignPattern/DataStructure/ListNode.go   DataStructure/StackAndQueue/ImplementCalculator.go -> ../Go-DesignPattern/DataStructure/StackAndQueue/ImplementCalculator.go   DataStructure/StackAndQueue/MinStack.go -> ../Go-DesignPattern/DataStructure/StackAndQueue/MinStack.go   DataStructure/StackAndQueue/TwoStacksMockQueue.go -> ../Go-DesignPattern/DataStructure/StackAndQueue/TwoStacksMockQueue.go   DataStructure/StackAndQueue/arrayImplementStack.go -> ../Go-DesignPattern/DataStructure/StackAndQueue/arrayImplementStack.go   DataStructure/StackAndQueue/doubleLinkedListImplementStack.go -> ../Go-DesignPattern/DataStructure/StackAndQueue/doubleLinkedListImplementStack.go   DataStructure/TreeNode.go -> ../Go-DesignPattern/DataStructure/TreeNode.go   DataStructure/dijkstraExercise.go -> ../Go-DesignPattern/DataStructure/dijkstraExercise.go   DataStructure/set/selfSet.go -> ../Go-DesignPattern/DataStructure/set/selfSet.go   DataStructure/skipList/skl.go -> ../Go-DesignPattern/DataStructure/skipList/skl.go

--- 22Intership/lists.go
+++ 22Intership/lists.go
@@ -23,7 +23,16 @@ func hasCycle2(head *ListNode) bool {
 	if head == nil || head.Next == nil {
 		return false
 	}
-	m := make(map[*ListNode]struct{})
+	m := make(map[*ListNode]struct{}, 0)
+	cur := head
+	for cur != nil {
+		if _, ok := m[cur]; ok {
+			return true
+		}
+		m[cur] = struct{}{}
+		cur = cur.Next
+	}
+	return false
 }
 func detechCycle(head *ListNode) (*ListNode, bool) {
 	if head == nil || head.Next == nil {
