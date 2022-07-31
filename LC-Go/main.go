3b37d24a6dacccab1760b09c39458e938e1fb107
flyingchase <zhongbingwang@yeah.net>
2022-07-31 15:35:07 +0800

    替换环境

--- LC-Go/main.go
+++ LC-Go/main.go
@@ -1,42 +1,38 @@
 package main
 
-import "fmt"
-
-package main
-
 import (
-"fmt"
+	"fmt"
 )
 
 func main() {
 	// var a int
 	// fmt.Scan(&a)
-	nums:=[]int{1,3,5,2,4,6,8,0}
-	res:=longestIncreasingSubArray(nums)
+	nums := []int{1, 3, 5, 2, 4, 6, 8, 0}
+	res := longestIncreasingSubArray(nums)
 
-fmt.Println(res)
+	fmt.Println(res)
 }
-func longestIncreasingSubArray(nums []int) []int{
-	if len(nums)==0 {
+func longestIncreasingSubArray(nums []int) []int {
+	if len(nums) == 0 {
 		return []int{}
 	}
-	if len(nums)==1 {
+	if len(nums) == 1 {
 		return nums
 	}
-	res :=[]int{}
-	for i:=0;i<len(nums)-1;i++ {
-		temp:=[]int{nums[i]}
-		j:=i+1
-		for ;j<len(nums);j++ {
-			if nums[j]>nums[i]{
+	res := []int{}
+	for i := 0; i < len(nums)-1; i++ {
+		temp := []int{nums[i]}
+		j := i + 1
+		for ; j < len(nums); j++ {
+			if nums[j] > nums[i] {
 				temp = append(temp, nums[j])
-			}else {
+			} else {
 				break
 			}
 		}
-		i=j
-		if len(res)<len(temp) {
-			res=append([]int{},temp...)
+		i = j
+		if len(res) < len(temp) {
+			res = append([]int{}, temp...)
 		}
 	}
 	return res
