e379e6774d5fb7ee495a4588cea7c20016b125c5
flyingchase <zhongbingwang@yeah.net>
2022-08-05 23:11:58 +0800

    新建秋招/提前批面试记录

---
+++ fuck/qushi/question_1.go
@@ -0,0 +1,25 @@
+package qushi
+
+import (
+	"strings"
+)
+
+func Q1(myTemplate string, keys, values []string) string {
+	if len(myTemplate) == 0 {
+		return myTemplate
+	}
+	mapping := make(map[string]string)
+	for i := 0; i < len(keys); i++ {
+		mapping[keys[i]] = values[i]
+	}
+	var count int
+	for i := 0; i < len(myTemplate); i++ {
+		if myTemplate[i] == '%' {
+			count++
+		}
+	}
+	s := strings.Split(myTemplate, "%")
+	s2 := strings.Join(s, "")
+	return s2
+
+}
